52004e05fcd6901af30485cdf6a5c3296223cbc1
hyony <runa369@naver.com>
2024-06-21 16:58:16 +0900

    Revert "레이아웃 분할"
    This reverts commit 3af876f20debc7ccb0f485069db3e83c555c7b2f.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-    <PageHeader/>   <!-- 헤더 컴포넌트 -->
-    <router-view/>  <!-- 페이지 이동이 표시될 곳 -->
-    <PageFooter/>   <!-- 푸터 컴포넌트 -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import PageHeader from './components/PageHeader.vue'
-import PageFooter from './components/PageFooter.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-name: 'App',
-components: {
-    PageHeader,
-    PageFooter
-}
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
 #app {
-font-family: Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/views/PageAbout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="about">
-        <h1>여기는 about 페이지입니다.</h1>
-    </div>
-</template>
