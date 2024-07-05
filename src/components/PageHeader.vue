114d2a86071cf064ec1a6320006397d175d8563c
hyony <runa369@naver.com>
2024-07-05 14:51:26 +0900

    헤더 변경

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,44 +1,24 @@
 <template>
     <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <div class="col-md-3 mb-2 mb-md-0">
-                <router-link to="/" class="d-inline-flex link-body-emphasis">
-                    <img src="../assets/img/plume_logo_temp.svg" alt="PLUME 로고" class="float-start"/>
-                </router-link>
-                |
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-                </form>
-            </div>
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><router-link to="/feed" class="nav-link px-2 link-secondary">Feed</router-link></li>
-                <li><router-link to="/recm" class="nav-link px-2">Recommend</router-link></li>
-                <li><router-link to="/rank" class="nav-link px-2">Rank</router-link></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2"><router-link to="/sign" class="nav-link px-2">Sign up</router-link></button>
-                <button type="button" class="btn btn-primary"><router-link to="/login" class="nav-link px-2">Login</router-link></button>
-            </div>
-        </header>
-        <br/>
-    </div>
-    <div class="container">
-        <header class="border-bottom">
-            <div class="row">
-                <div class="col-md-3">
-                    <router-link to="/" class="">
-                        <img src="../assets/img/plume_logo_temp.svg" alt="PLUME 로고" class="float-start"/>
+        <header class="d-flex justify-content-between align-items-center border-bottom py-3">
+            <div class="d-flex align-items-center">
+                <div class="border-right">
+                    <router-link to="/" class="fs-2 m-3">
+                        <i class="bi bi-search-heart"></i>
                     </router-link>
-                    <span class="">|</span>
-                    <input type="search" placeholder="Search..." aria-label="Search" class="">
                 </div>
-                <div class="col">2</div>
-                <div class="col-md-3">
-                    <button type="button" class="btn btn-outline-primary me-2"><router-link to="/sign" class="nav-link px-2">Sign up</router-link></button>
-                    <button type="button" class="btn btn-primary"><router-link to="/login" class="nav-link px-2">Login</router-link></button>
+                <div class="ms-3">
+                    <input type="search" placeholder="Search..." aria-label="Search" class="form-control ml-3"/>
                 </div>
+            </div>
+            <ul class="d-flex nav">
+                <li><router-link to="/feed" class="nav-link px-2" >Feed</router-link></li>
+                <li><router-link to="/recm" class="nav-link px-2" >Recommend</router-link></li>
+                <li><router-link to="/rank" class="nav-link px-2" >Rank</router-link></li>
+            </ul>
+            <div class="d-flex">
+                <button type="button" class="btn btn-outline-primary me-2"><router-link to="/sign" class="nav-link">Sign up</router-link></button>
+                <button type="button" class="btn btn-outline-primary"><router-link to="/login" class="nav-link">Login</router-link></button>
             </div>
         </header>
     </div>
@@ -46,10 +26,11 @@
 
 <script>
 export default {
-
+    
 }
+
 </script>
 
-<style>
-
+<style scoped>
+.border-right{ border-right: 1px solid #ccc;}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';  // router 객체 가져오기
 import 'bootstrap';             // bootstrap 객체 가져오기
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';  //bootsstrap 아이콘
 
 const app = createApp(App); 
 app.use(router);    // vue router를 app에 등록
