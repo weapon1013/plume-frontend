2d379ca3395210ddcc660305dbcc3793bb550a5b
hyony <runa369@naver.com>
2024-08-16 17:47:28 +0900

    헤더 검색창 수정

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,8 +4,13 @@
             <router-link to="/">
                 <Plumelogo style="margin-bottom: 20px;"></Plumelogo>
             </router-link>
-            <i class="pi pi-search"></i>
-            <input placeholder="검색창">
+            <div class="search-box flex align-items-center">
+                <InputText placeholder="검색창"/>
+                <button>
+                    <i class="pi pi-search" aria-hidden="true"></i>
+                </button>
+                
+            </div>
         </div>
         <div class="layout-topbar-mid">
             <ul class="list-none">
@@ -36,6 +41,7 @@
 import { ref } from 'vue';
 import LoginStatic from "./btn/LoginStaticHeader.vue";
 import Plumelogo from "./btn/PlumeLogo.vue";
+import InputText from 'primevue/inputtext';
 
 // loginStat: true(로그인) / false(비로그인)
 const loginCheck = ref(false);
@@ -47,4 +53,16 @@ const loginStat = ref(loginCheck);
 .list-none{ display: flex; align-items: center; }
 .list-none li{ display: flex; padding: .5rem 1rem; align-items: center; overflow: hidden; position:relative; }
 .list-none li a{ text-decoration-line: none; font-weight: 500; color: var(--primary-menu-color); }
+
+.search-box{ margin-left: 2rem; border-left: 2px solid #747d7d; height: 35px; padding: 0 0 0 2rem;}
+.search-box input{ width:12rem; height:2rem; transition: all 0.8s ease; }
+.search-box input:focus{ border: 1px solid #CBD5E1; }
+.search-box button{ width: 2rem; height: 2rem; left: -20px; position: relative;
+    background-color: #eff2fb; border: 1px solid #CBD5E1; border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+}
+
+
+
+
 </style>
